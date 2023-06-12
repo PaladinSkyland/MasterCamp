bcc6ff9c8b52612b932fcce3c84359e815b83154
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-12 14:04:43 +0200

    navigation bar and avatar

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Link to="/">Faire une demandes</Link>
+      <Link to="/contact">Mes demandes</Link>
+      <Avatar img="https://www.w3schools.com/howto/img_avatar.png" />
+    </div>
+  );
+};
+
+const Avatar = (props) => {
+  return (
+    <div className="Avatar" class="bg-sky-500 hover:bg-sky-700">
+      <img src={props.img} alt="avatar" />;
+    </div>
+  );
+};
 
 const ContactPage = () => {
   return (
     <div>
-      <h1>Page de contact</h1>
-      <Link to="/">Aller à la page home</Link>
+      <NavBar />
       {/* Contenu de la page de contact */}
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-
-app.get("/api", (req,res) => {
-    res.send({"test": "lucas"})
-})
+app.get("/api", (req, res) => {
+  res.send({ test: "lucas" });
+});
 
 app.listen(port, () => {
-    console.log("listening on port", port)
-})
+  console.log("listening on port", port);
+});
