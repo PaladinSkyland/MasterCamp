c2d2a1d7fed5ef129441a19c59f7d658ed6efee2
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-12 16:08:49 +0200

    ajout logo et button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,14 @@
-import React, {useState, useContext} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../AuthContext';
-
-
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthContext";
 
 
 const Login = () => {
-
   const { isLoggedIn, login, logout } = useContext(AuthContext);
-  const navigate = useNavigate()
- 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -22,50 +19,44 @@ const Login = () => {
   };
 
   const handleSubmit = (e) => {
-    console.log(isLoggedIn)
+    console.log(isLoggedIn);
     e.preventDefault();
 
-    if(username === "lucas" && password === "test"){
-      console.log("reussi")
-      login({name: username, password: password})
-      navigate('/contact', {replace: true})   
-
-
-    }else{
-      console.log("fail")
+    if (username === "lucas" && password === "test") {
+      console.log("reussi");
+      login({ name: username, password: password });
+      navigate("/contact", { replace: true });
+    } else {
+      console.log("fail");
     }
-
-    
-
   };
-  
+
   return (
     <div>
       <h1>Page d'accueil</h1>
       <Link to="/contact">Aller à la page de contact</Link>
       <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username : </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password : </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+        <div>
+          <label htmlFor="username">Username : </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password : </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
-    
   );
 };
 
