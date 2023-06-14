8cce441ccc4e05b3942f99d385133b88fb98be5f
Lucas Brancolini <lucas.brancolini@efrei.net>
2023-06-14 17:03:21 +0200

    removed proxy 6000

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -69,7 +69,6 @@ const LoginPage = () => {
       <button type="submit">Login</button>
     </form>
     <button onClick={HandleLogout}>logout</button>
-    <button onClick={navigate("/register")}>Register</button> 
     </div>
     
   );
