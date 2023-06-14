e8793ff0369400ef9d41e4c694cbdbfbfdae47db
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-14 15:42:13 +0200

    app de bug

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -36,91 +36,78 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>Page d'accueil</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="username">Username : </label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password : </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <button onClick={HandleLogout}>logout</button>
-    </div>
     <div class="md:flex flex-row overflow-hidden">
-    <div class="basis-2/5">
-      <div class="flex gap-x-4">
-        <img
-          className="h-12 w-13 flex-none rounded-full bg-gray-50"
-          src="logo.png"
-          alt="logo credit express"
-        />
-        <div className="min-w-0 flex-auto">
-          <p className="text-sm font-semibold leading-6 text-gray-900">
-            Credit Express
-          </p>
-        </div>
-      </div>
-      <form onSubmit={handleSubmit} class="block p-3">
-        <div class="m-3">
-          <label htmlFor="username">Username : </label>
-          <input
-            type="text"
-            id="username"
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-800 sm:text-sm sm:leading-6"
-            value={username}
-            placeholder="Votre nom"
-            onChange={handleUsernameChange}
+      <div class="basis-2/5">
+        <div class="flex gap-x-4">
+          <img
+            className="h-12 w-13 flex-none rounded-full bg-gray-50"
+            src="logo.png"
+            alt="logo credit express"
           />
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">
+              Credit Express
+            </p>
+          </div>
         </div>
-        <div class="m-3">
-          <label htmlFor="password">Password : </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            placeholder="Votre mot de passe"
-            class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div class="flex flex-col">
-          <Link to="/signin" class="text-blue-400">
-            New in Credit Express
-          </Link>
-          <button
-            class="bg-gradient-to-r
+        <form onSubmit={handleLogin} class="block p-3">
+          <div class="m-3">
+            <label htmlFor="username">Username : </label>
+            <input
+              type="text"
+              id="username"
+              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-800 sm:text-sm sm:leading-6"
+              value={username}
+              placeholder="Votre nom"
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div class="m-3">
+            <label htmlFor="password">Password : </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Votre mot de passe"
+              class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div class="flex flex-col">
+            <Link to="/signin" class="text-blue-400">
+              New in Credit Express
+            </Link>
+            <button
+              class="bg-gradient-to-r
         from-cyan-300 via-blue-500
         to-violet-300 rounded-full
         text-white 
         px-4 py-2
         hover:text-black
         "
-            type="submit"
-          >
-            Login
-          </button>
-        </div>
-      </form>
+              type="submit"
+            >
+              Login
+            </button>
+            <button
+              class="bg-gradient-to-r
+        from-cyan-300 via-blue-500
+        to-violet-300 rounded-full
+        text-white 
+        px-4 py-2
+        hover:text-black
+        "
+              onClick={HandleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class="basis-2/3">
+        <img class="" src="wallpaper.png" alt="wallpaper" />
+      </div>
     </div>
-    <div class="basis-2/3">
-      <img class="" src="wallpaper.png" alt="wallpaper" />
-    </div>
-  </div>
   );
 };
 
