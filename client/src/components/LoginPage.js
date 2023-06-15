654323e1880ff259c8c2d5146b04f6684a00575b
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-15 16:23:37 +0200

    modif css

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,17 +1,19 @@
-import React, {useState, useContext} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../authentification/AuthContext';
-import {login, setHttpOnlyCookie}  from '../authentification/login'
-import bcrypt from 'bcryptjs';
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../authentification/AuthContext";
+import { login, setHttpOnlyCookie } from "../authentification/login";
+import bcrypt from "bcryptjs";
 
+//TODO : Credit expresse en gros
+//TODO : image mettre en grand sur le pourcentage
+//TODO : rencentrer credit express
 
 const LoginPage = () => {
-
   const { setIsLoggedIn, logout } = useContext(AuthContext);
-  const navigate = useNavigate()
- 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -19,10 +21,9 @@ const LoginPage = () => {
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-  }; 
+  };
 
-  const handleLogin =  (e) => {
-
+  const handleLogin = (e) => {
     //Permet d'éviter le comportement de base du formulaire
     e.preventDefault();
 
@@ -32,48 +33,43 @@ const LoginPage = () => {
     const encryptedPassword = bcrypt.hashSync(password, salt);
 
     // Vérification de l'utilisateur
-    login(email, password).then(data => {
-
-      for (let key in data){
-        if (data[key] === "Identifiants invalides"){
-          alert("identifiants invalides") 
-          return
+    login(email, password).then((data) => {
+      for (let key in data) {
+        if (data[key] === "Identifiants invalides") {
+          alert("identifiants invalides");
+          return;
         }
       }
 
-      let token = data.token
+      let token = data.token;
 
       //On stocke le token en local, mais aussi dans un cookie HTTP only
-      localStorage.setItem('token', token);
-      setHttpOnlyCookie('token', token, new Date(Date.now() + (3600 * 1000))); // expiration dans 1 heure (en millisecondes)
+      localStorage.setItem("token", token);
+      setHttpOnlyCookie("token", token, new Date(Date.now() + 3600 * 1000)); // expiration dans 1 heure (en millisecondes)
 
       //On met le contexte login à true, et on se déplace sur la page voulue
-      setIsLoggedIn(true)
-      navigate('/contact')
-      
-    })
-  };
-
-  const HandleLogout = () => {
-    logout();
+      setIsLoggedIn(true);
+      navigate("/contact");
+    });
   };
 
   return (
-    <div class="md:flex flex-row overflow-hidden">
+    <div class="md:flex flex-row">
       <div class="basis-2/5">
         <div class="flex gap-x-4">
           <img
-            className="h-12 w-13 flex-none rounded-full bg-gray-50"
+            className="h-10 w-auto flex-none rounded-full bg-gray-50"
             src="logo.png"
             alt="logo credit express"
           />
-          <div className="min-w-0 flex-auto">
-            <p className="text-sm font-semibold leading-6 text-gray-900">
-              Credit Express
-            </p>
-          </div>
+          <p className="text-sm font-semibold leading-6 text-gray-900 my-auto">
+            Credit Express
+          </p>
         </div>
-        <form onSubmit={handleLogin} class="block p-3">
+        <div>
+          
+        </div>
+        <form onSubmit={handleLogin} class="block p-3 my-auto">
           <div class="m-3">
             <label htmlFor="email">Email : </label>
             <input
@@ -101,30 +97,10 @@ const LoginPage = () => {
               New in Credit Express
             </Link>
             <button
-              class="bg-gradient-to-r
-        from-cyan-300 via-blue-500
-        to-violet-300 rounded-full
-        text-white 
-        px-4 py-2
-        hover:text-black
-        my-3
-        "
+              class="bg-gradient-to-r from-cyan-300 via-blue-500 to-violet-300 rounded-full text-white px-4 py-2 hover:text-black my-3"
               type="submit"
             >
               Login
-            </button>
-            <button
-              class="bg-gradient-to-r
-        from-cyan-300 via-blue-500
-        to-violet-300 rounded-full
-        text-white 
-        px-4 py-2
-        my-3
-        hover:text-black
-        "
-              onClick={HandleLogout}
-            >
-              Logout
             </button>
           </div>
         </form>
--- client/src/components/RegisterPage.js
+++ client/src/components/RegisterPage.js
@@ -1,14 +1,13 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
 
 const RegisterPage = () => {
   //const navigate = useNavigate()
- 
-  const [username, setUsername] = useState('');
-  const [userfirstname, setUserfirstname] = useState('');
-  const [email, setemail] = useState('');
-  const [password, setPassword] = useState('');
- 
+
+  const [username, setUsername] = useState("");
+  const [userfirstname, setUserfirstname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
@@ -26,13 +25,12 @@ const RegisterPage = () => {
   };
 
   const handleSubmit = (e) => {
-
     e.preventDefault();
 
-    fetch('/signin', {
-      method: 'POST',
+    fetch("/signin", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         // Récupérez les valeurs du formulaire pour les envoyer au serveur
@@ -40,66 +38,63 @@ const RegisterPage = () => {
         userfirstname: userfirstname,
         email: email,
         password: password,
-      })
+      }),
     })
-      .then(response => response.json())
-      .then(data => {
-
+      .then((response) => response.json())
+      .then((data) => {
         // Gérer la réponse du serveur ici
         console.log(data);
       })
-      .catch(error => {
-        
+      .catch((error) => {
         // Gérer les erreurs ici
         console.error(error);
       });
   };
-  
+
   return (
     <div>
       <h1>Page de sign in</h1>
-      
+
       <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username : </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="userfirstname">FirstName : </label>
-        <input
-          type="text"
-          id="userfirstname"
-          value={userfirstname}
-          onChange={handleUserfirstnameChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">email : </label>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password : </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <button type="submit">Sign in</button>
-    </form>
+        <div>
+          <label htmlFor="username">Username : </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="userfirstname">FirstName : </label>
+          <input
+            type="text"
+            id="userfirstname"
+            value={userfirstname}
+            onChange={handleUserfirstnameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">email : </label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password : </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Sign in</button>
+      </form>
     </div>
-    
   );
 };
 
