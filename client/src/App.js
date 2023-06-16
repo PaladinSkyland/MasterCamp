901af9a92f635f49c8c59aac723c8ffd4a78095d
OUGO <hugo.parmentier@efrei.net>
2023-06-16 11:40:16 +0200

    page 404

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider,  } from "./context/AuthContext";
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import ContactPage from './components/HomePage';
+import ErrorPage from './components/ErrorPage';
 import { UserProvider } from "./context/UserContext";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<UserProvider><RegisterPage /></UserProvider>} />
           <Route path="/home" element={<UserProvider><ContactPage /></UserProvider>} />
+          <Route path="/error" element={<UserProvider><ErrorPage /></UserProvider>} />
         </Routes>
       </AuthProvider>
     </Router>
