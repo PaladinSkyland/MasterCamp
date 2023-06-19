c12dbf67334f3775bf2c7e60bf5c1cd5f0af39fb
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-19 14:40:22 +0200

    mise a jour des cotes

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,27 +1,24 @@
 import React, { createContext, useState } from "react";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+  const [userData, setUserData] = useState("");
 
-    const [userData, setUserData] = useState("");
+  const getOnglets = () => {
+    const type = userData.UserType;
 
-    const getOnglets = () => {
-
-      const type = userData.UserType
-
-      switch(type){
-        case 'customer':
-          return ["Demande de prêt", "Mes demandes"]
-        case 'employee':
-          return ["Demandes","Suivi demandes"]
-      }
+    switch (type) {
+      case "customer":
+        return ["Demande de prêt", "Mes demandes"];
+      case "employee":
+        return ["Demandes", "Suivi demandes"];
     }
+  };
 
-
-    return (
+  return (
     <UserContext.Provider value={{ userData, setUserData, getOnglets }}>
       {children}
     </UserContext.Provider>
-    )
-}
+  );
+};
