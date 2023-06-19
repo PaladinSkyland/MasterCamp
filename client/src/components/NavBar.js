7a6940c42a4768126242539cf54e1ce2396fb553
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-19 14:39:39 +0200

    nav bar modification

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,32 @@
 import React, { useContext, useEffect } from "react";
 import ComponentLogo from "./Logo";
 import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const { getOnglets } = useContext(UserContext);
 
-  const {getOnglets} = useContext(UserContext)
+  const onglets = getOnglets();
+  const navigate = useNavigate();
 
-  const onglets = getOnglets()
-  
-  
   return (
     <div className="h-16 flex justify-between shadow-md">
       <ComponentLogo />
-      <ul className=" ml-3 flex flex-row gap-x-1">
+      <ul className=" ml-3 flex flex-row gap-x-3 items-center">
         {onglets.map((onglet, index) => (
-          <li className="my-auto" key={index}>{onglet}</li>
+          <li
+            className="hover:bg-sky-100 p-2 rounded-md"
+            key={index}
+            onClick={() => navigate("/login")}
+          >
+            {onglet}
+          </li>
         ))}
         <img
-          className="h-12 w-13 flex-none my-auto"
+          className="h-12 w-13 flex-none my-auto hover:bg-sky-100 rounded-full"
           src="profile.png"
           alt="profile image"
+          onClick={() => navigate("/account")}
         />
       </ul>
     </div>
