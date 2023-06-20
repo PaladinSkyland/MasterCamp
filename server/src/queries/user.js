2f9bd10dffd86a8845d4df5655edb0af4a596771
OUGO <hugo.parmentier@efrei.net>
2023-06-20 12:20:36 +0200

    arrangé l authentification du coté serv

--- server/src/queries/user.js
+++ server/src/queries/user.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const db = require('../db') //Chemin vers les infos de connexion à la db
 
 exports.getUserInfoByID = function(id) {
@@ -16,4 +17,134 @@ exports.getUserInfoByID = function(id) {
       });
     });
   };
-  
+
+
+exports.userAlreadyExists = function(email) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT Email FROM Users WHERE Email = ?",
+      [email],
+      (error, results) => {
+        if (error) {
+          // Gérer les erreurs qui se produisent lors de l'exécution de la requête ou d'autres opérations asynchrones
+          console.error(error);
+          reject(new Error("Connection to DB failed"));
+        } else {
+          // Utiliser les résultats de la requête
+          if (results.length > 0) {
+            // L'utilisateur existe déjà, renvoyer une réponse d'erreur
+            //console.log("L'utilisateur existe déjà.");
+            resolve(true);
+          } else {
+            console.log("L'utilisateur n'existe pas.");
+            resolve(false);
+          }
+        }
+      }
+    );
+  });
+};
+
+const getBankbyName = function(name) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT ID_bank FROM Bank WHERE Name = ?",
+      [name],
+      (error, results) => {
+        if (error) {
+          // Gérer les erreurs qui se produisent lors de l'exécution de la requête ou d'autres opérations asynchrones
+          console.error(error);
+          reject(new Error("Connection to DB failed"));
+        } else {
+          // Utiliser les résultats de la requête
+          if (results.length > 0) {
+            // La banque est existante
+            resolve(results[0].ID_bank);
+          }
+          else{
+            reject(new Error("Bank not found"));
+          }
+        }
+      }
+    );
+  });
+};
+
+
+exports.userInsertInto  = function(email,encryptedPassword,username,userfirstname,type,bankname) {
+  return new Promise((resolve, reject) => {
+  if (type == "employee") {
+    db.query(
+      "INSERT INTO Users SET ?",
+      {
+        Email: email,
+        Password: encryptedPassword,
+        Name: username,
+        FirstName: userfirstname,
+        UserType: type
+      },
+      (error, results) => {
+        if (error) {
+          // Gérer les erreurs d'insertion dans la base de données
+          console.error(error);
+          reject(new Error("Une erreur s'est produite lors de l'enregistrement de l'utilisateur."));
+        } else {
+          console.log(
+            "Utilisateur inséré avec succès dans la base de données."
+          );
+          console.log("resultat de la requete : ", results.insertId);
+          const ID_user = results.insertId;
+          getBankbyName(bankname).then((ID_bank) => {
+            db.query(
+              "INSERT INTO Employee SET ?",
+              {
+                ID_user: ID_user,
+                ID_bank: ID_bank
+              },
+              (error, results) => {
+                if (error) {
+                  // Gérer les erreurs d'insertion dans la base de données
+                  console.error(error);
+                  reject(new Error("Une erreur s'est produite lors de l'enregistrement de l'utilisateur."));
+                } else {
+                  console.log(
+                    "Employé inséré avec succès dans la base de données."
+                  );
+                  resolve(true);
+                }
+              }
+            );
+          }
+          );
+        }
+
+      }
+    );
+  }
+  else {
+    db.query(
+      "INSERT INTO Users SET ?",
+      {
+        Email: email,
+        Password: encryptedPassword,
+        Name: username,
+        FirstName: userfirstname,
+      },
+      (error, results) => {
+        if (error) {
+          // Gérer les erreurs d'insertion dans la base de données
+          console.error(error);
+          reject(new Error("Une erreur s'est produite lors de l'enregistrement de l'utilisateur."));
+        } else {
+          console.log(
+            "Utilisateur inséré avec succès dans la base de données."
+          );
+          console.log("resultat de la requete : ", results.insertId);
+          resolve(true);
+        }
+
+      }
+    );
+  }
+}
+  )}
--- server/src/routes/authentification.js
+++ server/src/routes/authentification.js
@@ -5,6 +5,9 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const db = require('../db')
 
+const user = require("../queries/user");
+const e = require('express')
+
 router.post("/login", async (req,res) => {
 
   //Récupération du mdp et de l'email passé dans le formulaire de login
@@ -13,7 +16,7 @@ router.post("/login", async (req,res) => {
   //Récupération du mdp de l'utilisateur dans la BDD
 
 
-  const request = db.query("SELECT Password, ID_user from Users where Email = ?", [email], (error, results) => {
+  const request = await db.query("SELECT Password, ID_user from Users where Email = ?", [email], (error, results) => {
     if (error) {
       console.log(error);
     } else {
@@ -40,6 +43,46 @@ router.post("/register", async (req, res) => {
 
     //Effectuer la requête à la base de données pour obtenir le mot de passe de l'utilisateur
     //vérification si user déjà existant
+
+    const userAlreadyExists = user.userAlreadyExists(
+      email
+    );
+    userAlreadyExists.then((result) => {
+
+      if (result) {
+        // L'utilisateur existe déjà, renvoyer une réponse d'erreur
+        return res
+          .status(500)
+          .json({ message: "L'utilisateur existe déjà." });
+      } else {
+        const saltRounds = parseInt(process.env.cryptedKey);
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const encryptedPassword = bcrypt.hashSync(password, salt);
+        const userInsertInto = user.userInsertInto(
+          email,
+          encryptedPassword,
+          username,
+          userfirstname,
+          type,
+          bankref
+        );
+        //affcier la réponse de userInsertInto
+        //userInsertInto est une promesse
+        //affichage des erreurs si il y en a
+        
+        userInsertInto.then((result) => {
+          if (result) {
+            // L'utilisateur existe déjà, renvoyer une réponse d'erreur
+            return res.status(500).json({message : "Employé inséré"})
+          }
+        });
+      }
+    });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Erreur lors de l'inscription" });
+    }
+    /*
     db.query(
       "SELECT Email FROM Users WHERE Email = ?",
       [email],
@@ -159,10 +202,7 @@ router.post("/register", async (req, res) => {
           }
         }
       }
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    );*/
 });
 
 router.get("/getBanks", async (req, res) => {
