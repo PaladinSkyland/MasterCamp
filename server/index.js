1268550d5504d7a5e195e229edad14ed5dafff13
ludovic <ludovic.liu.chi.pioa@efrei.net>
2023-06-23 16:58:07 +0200

    increased request max payload size

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const port = 5000
 const userQueries = require('./src/queries/user')
 const db = require('./src/db') //Chemin vers les infos de connexion à la db
 const authenticateToken = require('./src/authenticateToken')
+//npm install body-parser
+const bodyParser = require('body-parser');
+
+//increase the maximum request payload size
+const MAX_PAYLOAD_SIZE = '16mb';
 
 app.use(express.json()); //Middleware express
+
+//configure body-parser middleware with increased limit
+app.use(bodyParser.json({ limit: MAX_PAYLOAD_SIZE }));
+app.use(bodyParser.urlencoded({ limit: MAX_PAYLOAD_SIZE, extended: true }));
 
 const authentificationRouter = require('./src/routes/authentification');
 app.use('/authentification', authentificationRouter)
