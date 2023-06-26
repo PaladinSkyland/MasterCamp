2c4a15e47200801d4d6448e6452ce9584b8f98af
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-26 16:51:00 +0200

    merge main

--- client/src/components/common/HomePage.js
+++ client/src/components/common/HomePage.js
@@ -1,12 +1,10 @@
 import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserContext";
-import { useNavigate } from "react-router-dom";
 import NavBar from "../NavBar";
 import ErrorPage from "./ErrorPage";
 
 const HomePage = () => {
   const banque = ["Bank1", "Bank2", "Bank3", "Bank4", "Bank5", "Bank6"];
-  const navigate = useNavigate();
   const { userData, setUserData } = useContext(UserContext);
 
   const storedToken = localStorage.getItem("token");
@@ -23,7 +21,7 @@ const HomePage = () => {
           setUserData(data);
         });
     }
-  },[]);
+  }, []);
 
   return (
     /* Si connecté */
@@ -31,18 +29,8 @@ const HomePage = () => {
       <div>
         <NavBar />
         <div className="m-3">
-          <h1 className="">Bienvenue {userData.Name}</h1>
-          <h2>Vous etes connecté en tant que {userData.UserType}</h2>
-        </div>
-        <div className="w-full">
-          <h1>Voici les banques disponibles :</h1>
-          <div className="flex flex-row gap-x-10">
-            {banque.map((onglet, index) => (
-              <p className="" key={index} onClick={() => navigate("/login")}>
-                {onglet}
-              </p>
-            ))}
-          </div>
+          <h1 className="text-4xl text-blue-500">Bienvenue {userData.Name}</h1>
+          <h2>Vous êtes connecté en tant que {userData.UserType}</h2>
         </div>
       </div>
     ) : (
