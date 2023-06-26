f3412f88a174c0280b8e3414afe73b17bce2137a
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-26 17:24:41 +0200

    changement de text

--- client/src/components/common/WelcomePage.js
+++ client/src/components/common/WelcomePage.js
@@ -65,7 +65,7 @@ const WelcomePage = () => {
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Everything you need to deploy your app
             </p>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8">
               Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
               Suspendisse eget egestas a elementum pulvinar et feugiat blandit
               at. In mi viverra elit nunc.
