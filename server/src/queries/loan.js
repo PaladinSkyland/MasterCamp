5a8276672264bac42e2acbf5cc09e64d0a9c03e6
Lucas Brancolini <lucas.brancolini@efrei.net>
2023-06-28 16:37:21 +0200

    Bug fix

--- server/src/queries/loan.js
+++ server/src/queries/loan.js
@@ -20,10 +20,9 @@ exports.insertLoan = function(interestRate,loanDuration,loanAmount,interestType,
 }
 
 exports.getLoansWithoutBank = function (sqlToAdd) {
-    request = sqlToAdd ? "SELECT users.Name, users.FirstName, loanapplications.* FROM loanapplications JOIN users USING (ID_user) WHERE ID_bank IS NULL AND ID_application NOT IN (" + sqlToAdd + ")" :
-    "SELECT users.Name, users.FirstName, loanapplications.* FROM loanapplications JOIN users USING (ID_user) WHERE ID_bank IS NULL" 
+    request = sqlToAdd ? "SELECT users.LastName, users.FirstName, Loanapplications.* FROM loanapplications JOIN users USING (ID_user) WHERE ID_bank IS NULL AND ID_application NOT IN (" + sqlToAdd + ")" :
+    "SELECT users.LastName, users.FirstName, LoanApplications.* FROM LoanApplications JOIN users USING (ID_user) WHERE ID_bank IS NULL" 
     return new Promise ((resolve,reject) => {
-<<<<<<< HEAD
         db.query(request, (error, result) => {
             if(error){
                 reject(error)
@@ -37,10 +36,7 @@ exports.getLoansWithoutBank = function (sqlToAdd) {
 exports.getLoansWithBank = function (ID_bank, sqlToAdd) {
         request = sqlToAdd ? "AND ID_application NOT IN (" + sqlToAdd + ")" : ""
         return new Promise ((resolve,reject) => {
-        db.query("SELECT users.Name, users.FirstName, loanapplications.* FROM loanapplications JOIN users USING (ID_user) WHERE ID_bank = ? " + request, [ID_bank], (error, result) => {
-=======
-        db.query("SELECT users.Name, users.FirstName, LoanApplications.* FROM LoanApplications JOIN users USING (ID_user) WHERE ID_bank IS NULL", (error, result) => {
->>>>>>> main
+        db.query("SELECT users.LastName, users.FirstName, LoanApplications.* FROM LoanApplications JOIN users USING (ID_user) WHERE ID_bank = ? " + request, [ID_bank], (error, result) => {
             if(error){
                 reject(error)
             }else{
