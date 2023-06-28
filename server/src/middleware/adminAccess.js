80c73655eb71b7c532810e7b6b07614787fea682
Kurnoth <arnaud.kohler@bbox.fr>
2023-06-28 11:29:28 -0100

    GetLoans for customer

--- server/src/middleware/adminAccess.js
+++ server/src/middleware/adminAccess.js
@@ -5,12 +5,14 @@ function adminAccess(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
+  console.log(token)
   if (token == null) {
     return res.sendStatus(401);
   }
 
   const UserType = jwt.verify(token, process.env.secretKey)
 
+  console.log(UserType)
   if (UserType.UserType !== "admin") {
     return res.sendStatus(401);
   }
