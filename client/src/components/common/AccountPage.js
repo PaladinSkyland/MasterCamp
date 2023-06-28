00d664e2fbf5e9206fd5a2393c9650154cc71718
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-28 16:26:43 +0200

    replace class by className

--- client/src/components/common/AccountPage.js
+++ client/src/components/common/AccountPage.js
@@ -9,51 +9,51 @@ const AccountPage = () => {
 
   const UserInfomation = (props) => {
     return (
-      <section class="h-screen bg-gray-100/50 m-4">
-        <form class="container max-w-2xl mx-auto shadow-md md:w-3/4">
-          <div class="p-4 border-t-2 border-blue-400 rounded-lg bg-gray-100/5 ">
-            <div class="max-w-sm mx-auto md:w-full md:mx-0">
-              <div class="inline-flex items-center space-x-4">
-                <h1 class="text-blue-500 text-4xl">My account</h1>
+      <section className="h-screen bg-gray-100/50 m-4">
+        <form className="container max-w-2xl mx-auto shadow-md md:w-3/4">
+          <div className="p-4 border-t-2 border-blue-400 rounded-lg bg-gray-100/5 ">
+            <div className="max-w-sm mx-auto md:w-full md:mx-0">
+              <div className="inline-flex items-center space-x-4">
+                <h1 className="text-blue-500 text-4xl">My account</h1>
               </div>
             </div>
           </div>
-          <div class="space-y-6 bg-white">
-            <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-1/3">
+          <div className="space-y-6 bg-white">
+            <div className="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
+              <h2 className="max-w-sm mx-auto md:w-1/3">
                 Account: <span className="text-xl">{userData.UserType}</span>
               </h2>
-              <div class="max-w-sm mx-auto md:w-2/3">
-                <div class="relative ">
+              <div className="max-w-sm mx-auto md:w-2/3">
+                <div className="relative ">
                   <input
                     type="text"
                     id="user-info-email"
-                    class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
                     placeholder={userData.Email}
                   />
                 </div>
               </div>
             </div>
             <hr />
-            <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-1/3">Personal info</h2>
-              <div class="max-w-sm mx-auto space-y-5 md:w-2/3">
+            <div className="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
+              <h2 className="max-w-sm mx-auto md:w-1/3">Personal info</h2>
+              <div className="max-w-sm mx-auto space-y-5 md:w-2/3">
                 <div>
-                  <div class=" relative ">
+                  <div className=" relative ">
                     <input
                       type="text"
                       id="user-info-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
                       placeholder={userData.LastName}
                     />
                   </div>
                 </div>
                 <div>
-                  <div class=" relative ">
+                  <div className=" relative ">
                     <input
                       type="text"
                       id="user-info-first-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
                       placeholder={userData.FirstName}
                     />
                   </div>
@@ -61,32 +61,32 @@ const AccountPage = () => {
               </div>
             </div>
             <hr />
-            <div class="items-center w-full p-8 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-4/12">Change password</h2>
-              <div class="w-full max-w-sm pl-2 mx-auto space-y-5 md:w-5/12 md:pl-9 md:inline-flex">
-                <div class=" relative ">
+            <div className="items-center w-full p-8 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
+              <h2 className="max-w-sm mx-auto md:w-4/12">Change password</h2>
+              <div className="w-full max-w-sm pl-2 mx-auto space-y-5 md:w-5/12 md:pl-9 md:inline-flex">
+                <div className=" relative ">
                   <input
                     type="text"
                     id="user-info-password"
-                    class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
                     placeholder="Password"
                   />
                 </div>
               </div>
-              <div class="text-center md:w-3/12 md:pl-6">
+              <div className="text-center md:w-3/12 md:pl-6">
                 <button
                   type="button"
-                  class="py-2 px-4  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  className="py-2 px-4  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
                   Change
                 </button>
               </div>
             </div>
             <hr />
-            <div class="w-full flex md:flex-col px-4 pb-4 ml-auto text-gray-500 md:w-1/2">
+            <div className="w-full flex md:flex-col px-4 pb-4 ml-auto text-gray-500 md:w-1/2">
               <button
                 type="submit"
-                class="py-2 px-4  btn-primary hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                className="py-2 px-4  btn-primary hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
                 Save
               </button>
@@ -104,7 +104,7 @@ const AccountPage = () => {
       <UserInfomation>
         <button
           onClick={logout}
-          class="py-2 px-4 btn-primary hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+          className="py-2 px-4 btn-primary hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
         >
           Logout
         </button>
