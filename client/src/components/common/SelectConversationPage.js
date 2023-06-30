6a3591bea9ca59f33dda32df6a4831ff218de3c2
OUGO <hugo.parmentier@efrei.net>
2023-06-30 15:51:59 +0200

    SelectConv fix import

--- client/src/components/common/SelectConversationPage.js
+++ client/src/components/common/SelectConversationPage.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SelectConversationPage = () => {
   const [conversations, setConversations] = useState([]);
   const storedToken = localStorage.getItem("token");
-  const loanID = "";
   const urlParams = new URLSearchParams(window.location.search);
   const application = urlParams.get('application');
 
