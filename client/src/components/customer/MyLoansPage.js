31c4b4743e3c44c1c65204ba874a75f2611db3dc
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-30 17:34:46 +0200

    desing loanapplication

--- client/src/components/customer/MyLoansPage.js
+++ client/src/components/customer/MyLoansPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from '../NavBar';
 const storedToken = localStorage.getItem('token');
 
@@ -24,21 +25,153 @@ const MyLoansPage = () => {
     fetchLoan();
   }, []);
 
-  return myLoanList ? (
+  const LoanDate = ({ date }) => {
+    const formattedDate = new Date(date).toLocaleDateString('fr-FR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    const today = new Date().toLocaleDateString('fr-FR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    if (formattedDate === today) {
+      return (
+        <span className="text-gray-500">
+          Aujourd'hui à {new Date(date).getHours()}:
+          {new Date(date).getUTCMinutes()}
+        </span>
+      );
+    } else {
+      return <span className="text-gray-500">{formattedDate}</span>;
+    }
+  };
+
+  const LoanStatus = ({ status }) => {
+    let statusStyle = '';
+
+    switch (status) {
+      case 'Pending':
+        statusStyle = 'orange';
+        break;
+      case 'Accepted':
+        statusStyle = 'green';
+        break;
+      case 'Finished':
+        statusStyle = 'blue';
+        break;
+      case 'Canceled':
+        statusStyle = 'red';
+        break;
+      default:
+        statusStyle = 'gray';
+    }
+
+    return (
+      <span
+        className={`mr-3 text-${statusStyle}-500 bg-${statusStyle}-100 font-bold p-2 rounded-full`}
+      >
+        {status}{' '}
+      </span>
+    );
+  };
+
+  const ChevronButton = ({ children }) => {
+    const [isToggled, setIsToggled] = useState(true);
+
+    const handleChange = () => {
+      setIsToggled(!isToggled);
+    };
+
+    return isToggled ? (
+      <svg
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 cursor-pointer"
+        onClick={handleChange}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+        />
+      </svg>
+    ) : (
+      <div>
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-6 h-6  cursor-pointer"
+          onClick={handleChange}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
+          />
+        </svg>
+        {children}
+      </div>
+    );
+  };
+
+  return (
     <div>
       <NavBar />
-      <ul>
-        {myLoanList.map((myLoan, index) => (
-            <li key={index}>
-              <p>{console.log(myLoan)}</p>
-            {myLoan.ID_application}
-            {myLoan.Status}
-            {Date(myLoan.Creation_date)}
-          </li>
-        ))}
-      </ul>
+      <div className="mt-8">
+        {myLoanList.length > 0 ? (
+          <ul className="space-y-4">
+            {myLoanList.map((myLoan, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between shadow rounded-lg py-2 px-4"
+              >
+                <div className="flex flex-col">
+                  <LoanDate date={myLoan.Creation_date} />
+                  <div className="flex flex-row m-2 space-x-4 items-center">
+                    <LoanStatus status={myLoan.Status} />
+                    <span className="font-bold">{myLoan.Amount}$</span>
+                  </div>
+                </div>
+                <ChevronButton>
+                  <div className="">
+                    <span className="font-bold">ID : hello</span>
+                  </div>
+                </ChevronButton>
+                <Link
+                  to={`/conversation?application=${myLoan.ID_application}`}
+                  className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                >
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+                    />
+                  </svg>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-gray-500 italic">Aucune demande de prêt</p>
+        )}
+      </div>
     </div>
-  ) : null;
+  );
 };
 
 export default MyLoansPage;
