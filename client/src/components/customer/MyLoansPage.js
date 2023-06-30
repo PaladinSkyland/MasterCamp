8f06fe8ace2b0b6ca1704633d821afee38ced05a
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-30 11:53:45 +0200

    debut de stylie

--- client/src/components/customer/MyLoansPage.js
+++ client/src/components/customer/MyLoansPage.js
@@ -1,42 +1,44 @@
-import React, {useState, useEffect} from "react";
-import NavBar from "../NavBar";
-const storedToken = localStorage.getItem("token");
+import React, { useState, useEffect } from 'react';
+import NavBar from '../NavBar';
+const storedToken = localStorage.getItem('token');
 
 const MyLoansPage = () => {
-    
-    const [myLoanList, setMyLoanList] = useState([]);
+  const [myLoanList, setMyLoanList] = useState([]);
 
-    useEffect(() => {
-        const fetchLoan = async () => {
-            try {
-                const response = await fetch("/customer/getMyLoans", {
-                    method: "GET",
-                    headers: {
-                    Authorization: `Bearer ${storedToken}`,
-                    "Content-Type": "application/json",
-                    },
-                });
-                const data = await response.json();
-                setMyLoanList(data);
-            } catch (error) {
-                console.error("Une erreur s'est produite :", error);
-            }
-        };
-        fetchLoan();
-    }, [])
+  useEffect(() => {
+    const fetchLoan = async () => {
+      try {
+        const response = await fetch('/customer/getMyLoans', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setMyLoanList(data);
+      } catch (error) {
+        console.error("Une erreur s'est produite :", error);
+      }
+    };
+    fetchLoan();
+  }, []);
 
-    return myLoanList ? (<div>
-        <NavBar/>
-        <ul>
+  return myLoanList ? (
+    <div>
+      <NavBar />
+      <ul>
         {myLoanList.map((myLoan, index) => (
-            <li key = {index}>
-                {myLoan.ID_application}
-                {myLoan.Status}
-                {myLoan.Creation_date}
-            </li>
+            <li key={index}>
+              <p>{console.log(myLoan)}</p>
+            {myLoan.ID_application}
+            {myLoan.Status}
+            {Date(myLoan.Creation_date)}
+          </li>
         ))}
-        </ul>
-    </div>) : ( null)
+      </ul>
+    </div>
+  ) : null;
 };
 
 export default MyLoansPage;
