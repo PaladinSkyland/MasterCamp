34fba2995733d18c8302f3b604f880991de506da
OUGO <hugo.parmentier@efrei.net>
2023-06-30 15:52:54 +0200

    Register Page import fixe

--- client/src/components/common/RegisterPage.js
+++ client/src/components/common/RegisterPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from 'react-router-dom';
 import ComponentLogo from "../Logo.js";
 const bcrypt = require("bcryptjs");
 
