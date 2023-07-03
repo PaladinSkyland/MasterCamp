6dd7a7c75e07eeb31260ac8db756c09db7aa652e
ludovic <ludovic.liu.chi.pioa@efrei.net>
2023-07-03 11:12:22 +0200

    added download, fixed upload

--- server/src/routes/customer.js
+++ server/src/routes/customer.js
@@ -27,52 +27,72 @@ const storage = multer.diskStorage({
 //create multer instance with the storage configuration
 const upload = multer({ storage });
 
-//generate a random encryption key and initialization vector (IV)
-/*
-const encryptionKey = crypto.randomBytes(32); //256 bits key
-const iv = crypto.randomBytes(16);
-
-const keyHex = encryptionKey.toString('hex');
-const ivHex = iv.toString('hex');
-*/
-
-//get the encryption key and the IV from the .env file 
-const encryptionKey = Buffer.from(process.env.encryptionKey, 'hex');
-const iv = Buffer.from(process.env.IV, 'hex')
-
-//create a cipher using the encryption key
-const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, iv);
-
 router.post("/upload", authenticateToken, customerAccess, upload.single('file'), async (req, res) => {
     const file = req.file; //uploaded file
     const type = req.body.fileType; //selected option
     const name = req.body.fileName; //file name
     const userID = req.user.ID_user; //user ID
 
-    console.log("user ID", userID)
+    const fileExist = fileQueries.CheckIfFileExist(type, userID);
 
-    const inputFile = "uploads\\" + name;
+    fileExist.then((response) => {
+        if (response.length == 0)
+            fileQueries.DeleteFile(type, userID);
+    });
+
+    //get the file to cipher
+    const inputFile = "uploads/" + name;
     const inputBuffer = fs.readFileSync(inputFile);
 
-    // Encrypt the file contents
-    const encryptedBuffer = Buffer.concat([cipher.update(inputBuffer), cipher.final()]);
+    //get the encryption key and the IV from the .env file 
+    const cipherKey = Buffer.from(process.env.cipherKey, 'hex');
+    const iv = Buffer.from(process.env.IV, 'hex');
 
-    // Write the encrypted contents to a new file
-    const outputFile = "uploads\\" + name;
-    fs.writeFileSync(outputFile, encryptedBuffer);
+    //create a cipher using the encryption key
+    const cipher = crypto.createCipheriv('aes-256-cbc', cipherKey, iv);
+
+    //cipher the file contents
+    const cipheredBuffer = Buffer.concat([cipher.update(inputBuffer), cipher.final()]);
+
+    if (!fs.existsSync("files/" + userID))
+    try {
+        fs.mkdir('files/' + userID, { recursive: true });
+        /*
+        fs.mkdir("files/" + userID, { recursive: true }, (error) => {
+            if (error) {
+                console.error('Erreur création du répertoire utilisateur', error);
+            } else {
+                console.log('Création du répertoire utilisateur');
+            }
+        });
+        */
+    }
+    catch {
+        console.error('Upload : Erreur création du répertoire utilisateur', error);
+    }
+
+    //write the ciphered contents to a new file
+    const outputFile = "files/" + userID + "/" + name;
+    try {
+        fs.writeFileSync(outputFile, cipheredBuffer);
+    }
+    catch {
+        console.error('Upload : Erreur écriture du fichier chiffré')
+    }
     
     try {
         //inserting into the DB
-        const fileInsertInto = fileQueries.fileInsertInto(name, type, file, userID);
+        const fileInsertInto = fileQueries.fileInsertInto(name, type, outputFile, userID);
 
         //return the result of the insertion request
         fileInsertInto.then((result) => {
             if (result) {
                 fs.unlink(file.path, (error) => {
                     if (error) {
-                      console.error('Error deleting temporary file on the server : ', error);
-                    } else {
-                      console.log('Temporary file on the server deleted successfully');
+                      console.error('Upload : Erreur supression du fichier temporaire : ', error);
+                    } 
+                    else {
+                      console.log('Upload : Fichier temporaire supprimé');
                     }
                 })
                 return res.status(200).send("Fichier ajouté avec succès");
@@ -82,14 +102,79 @@ router.post("/upload", authenticateToken, customerAccess, upload.single('file'),
     catch (error) {
         res.status(500).json({error: "Erreur lors de l'insertion du fichier"});
     }
-})
+});
 
 router.get('/files', authenticateToken, customerAccess, async (req, res) => {
-    const response = fileQueries.SelectFileByUserID(req.user.ID)
+    //get the files by ID user
+    const response = fileQueries.SelectFileByUserID(req.user.ID_user);
 
     response.then(response => {
-        res.json(response)
-    })
+       res.json(response);
+    });
+});
+
+router.get('/download/:fileType', authenticateToken, customerAccess, async (req, res) => {
+    //get the encryption key and the IV from the .env file 
+    const cipherKey = Buffer.from(process.env.cipherKey, 'hex');
+    const iv = Buffer.from(process.env.IV, 'hex');
+
+    //create a decipher using the encryption key
+    const decipher = crypto.createDecipheriv('aes-256-cbc', cipherKey, iv);
+
+    try {
+        //get file path with the user ID and the file type
+        const response = fileQueries.SelectFilePathByFileTypeAndUserID(req.user.ID_user, req.params.fileType);
+
+        response.then((response) => {
+            //decipher the file
+            //get the file to decipher
+            const inputFile = response[0].File_path;
+            const inputBuffer = fs.readFileSync(inputFile);
+
+            //decipher the file contents
+            const decipheredBuffer = Buffer.concat([decipher.update(inputBuffer), decipher.final()]);
+
+            //write the deciphered contents to a new file
+            if (!fs.existsSync("downloads/" + req.user.ID_user))
+                try {
+                    fs.mkdirSync('downloads/' + req.user.ID_user, { recursive: true });
+                }
+                catch {
+                    console.error('Download : Erreur création du répertoire utilisateur');
+                }
+            
+            const outputFile = "downloads/" + req.user.ID_user + "/" + response[0].Title;
+            try {
+                fs.writeFileSync(outputFile, decipheredBuffer)
+            }
+            catch (err) {
+                console.error('Download : Erreur écriture du fichier déchiffré : ', err);
+            }
+
+            //send file to download
+            //set the appropriate headers for the file download
+            res.setHeader('Content-Type', 'application/octet-stream');
+            res.setHeader('Content-Disposition', `attachment; filename="${response[0].Title}"`);
+
+            //stream the file to the response
+            const fileStream = fs.createReadStream(outputFile);
+            fileStream.pipe(res);
+
+            //delete the temporary file after sending it
+            fileStream.on('end', () => {
+                fs.unlink(outputFile, (error) => {
+                    if (error) {
+                        console.error('Download : Erreur suppression du fichier temporaire : ', error);
+                    } else {
+                        console.log('Download : Fichier temporaire supprimé');
+                    }
+                });
+            });
+        })
+    }
+    catch {
+        res.status(500).json({error: "Erreur lors de la récupération du fichier"});
+    }
 })
 
 router.post('/newLoan', authenticateToken, customerAccess, async (req, res) => {
